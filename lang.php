6dadd0a70d34efbaec519f47bdbafc65c6d43ef0
Ali Hasan <alihasan.iubat@gmail.com>
2018-10-25 21:48:53 +0600

    added lang file

---
+++ lang.php
@@ -0,0 +1,34 @@
+<?php  
+
+/** Translate Day & Month in your language */
+$Language_Translation = array(
+/** Days */
+	"sunday" => "Domingo",
+	"monday" => "Segunda-feira",
+	"tuesday" => "Terça-feira",
+	"wednesday" => "Quarta-feira",
+	"thursday" => "Quinta-feira",
+	"friday" => "Sexta-feira",
+	
+/** Months */
+	"january" => "Jan",
+	"february" => "Fev",
+	"march" => "Mar",
+	"april" => "Abr",
+	"may" => "Mai",
+	"june" => "Jun",
+	"july" => "Jul",
+	"august" => "Ago",
+	"september" => "Set",
+	"october" => "Out",
+	"november" => "Nov",
+	"december" => "Dez",
+
+/** Others */
+	"days" => "Dias",
+	"hours" => "Horas",
+	"minutes" => "Minutos",
+	"seconds" => "Segundos", 
+	"am" => "AM",
+	"pm" => "PM",
+);
